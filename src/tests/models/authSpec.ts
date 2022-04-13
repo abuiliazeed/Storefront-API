69ad410723dacd105ccdee9fb364bf5aa6873339
abuiliazeed <abuiliazeed@gmail.com>
2022-04-13 17:00:23 +0200

    test formatting

--- src/tests/models/authSpec.ts
+++ src/tests/models/authSpec.ts
@@ -10,41 +10,44 @@ describe('Authentication Module', () => {
       expect(userModel.authenticateUser).toBeDefined()
     })
   })
+
+  describe('Test Authentication Logic', () => {
+    const user = {
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'johndoe@gmail.com',
+      password: 'password'
+    } as User
+  
+    beforeAll(async () => {
+      const createdUser = await userModel.createUser(user)
+      user.id = createdUser.id
+    })
+  
+    afterAll(async () => {
+      const connection = await dbclient.connect()
+      // alter sequence id to 1
+      await connection.query('ALTER SEQUENCE users_id_seq RESTART WITH 1')
+      await connection.query('DELETE FROM users;')
+      connection.release()
+    })
+  
+    it('should authenticate a user', async () => {
+      const authenticatedUser = await userModel.authenticateUser(user.email, user.password)
+      expect(authenticatedUser?.email).toBe(user.email)
+      expect(authenticatedUser?.firstname).toBe(user.firstname)
+      expect(authenticatedUser?.lastname).toBe(user.lastname)
+    })
+  
+    it('Authenticate method should return null for wrong credentials', async () => {
+      const authenticatedUser = await userModel.authenticateUser(
+        'hamada@hamada.com',
+        'wrongpassword'
+      )
+      expect(authenticatedUser).toBe(null)
+    })
+  })
+  
+
 })
 
-describe('Test Authentication Logic', () => {
-  const user = {
-    firstname: 'John',
-    lastname: 'Doe',
-    email: 'johndoe@gmail.com',
-    password: 'password'
-  } as User
-
-  beforeAll(async () => {
-    const createdUser = await userModel.createUser(user)
-    user.id = createdUser.id
-  })
-
-  afterAll(async () => {
-    const connection = await dbclient.connect()
-    // alter sequence id to 1
-    await connection.query('ALTER SEQUENCE users_id_seq RESTART WITH 1')
-    await connection.query('DELETE FROM users;')
-    connection.release()
-  })
-
-  it('should authenticate a user', async () => {
-    const authenticatedUser = await userModel.authenticateUser(user.email, user.password)
-    expect(authenticatedUser?.email).toBe(user.email)
-    expect(authenticatedUser?.firstname).toBe(user.firstname)
-    expect(authenticatedUser?.lastname).toBe(user.lastname)
-  })
-
-  it('Authenticate method should return null for wrong credentials', async () => {
-    const authenticatedUser = await userModel.authenticateUser(
-      'hamada@hamada.com',
-      'wrongpassword'
-    )
-    expect(authenticatedUser).toBe(null)
-  })
-})
