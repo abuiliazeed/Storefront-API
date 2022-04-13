47256f0fb689854798dcd9dc56f48b99225359c3
abuiliazeed <abuiliazeed@gmail.com>
2022-04-13 12:11:28 +0200

    added users update with authentication

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,12 +1,11 @@
-import {Response, Request, NextFunction} from 'express'
+import { Response, Request, NextFunction } from 'express'
 import Error from '../interfaces/error.interface'
 
-
 const errorMiddleWare = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    const status = err.status || 500
-    const message = err.message || 'Something went wrong'
+  const status = err.status || 500
+  const message = err.message || 'Something went wrong'
 
-    res.status(status).json({status,message})
+  res.status(status).json({ status, message })
 }
 
 export default errorMiddleWare
--- src/routes/api/users.routes.ts
+++ src/routes/api/users.routes.ts
@@ -1,10 +1,13 @@
-import { Router, Request, Response } from "express"
-import {createUser,getAllUsers,getUserById,updateUser,deleteUser,authenticateUser} from '../../controllers/user.controllers'
+import { Router, Request, Response } from 'express'
+import {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  authenticateUser
+} from '../../controllers/user.controllers'
 import authenticationMiddleware from '../../middleware/authenticate.middleware'
-
-
-
-
 
 const userRoutes = Router()
 //create user route
@@ -14,7 +17,7 @@ userRoutes.get('/', getAllUsers)
 //get user by id route
 userRoutes.get('/:id', getUserById)
 //update user route
-userRoutes.put("/:id",authenticationMiddleware,updateUser)
+userRoutes.put('/:id', authenticationMiddleware, updateUser)
 //delete user route
 userRoutes.delete('/:id', deleteUser)
 //authenticate user route
